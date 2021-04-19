3c22229640a0379e2020d454c11e7e1fbe6a3113
badnameok <jjpocharatf2@gmail.com>
2021-04-19 23:48:10 +0700

    Added Terraraiser

---
+++ src/tower/TerraRaiser.java
@@ -0,0 +1,60 @@
+package tower;
+
+import logic.misc.Coordinate;
+import logic.towers.BaseTower;
+
+public class TerraRaiser extends BaseTower{
+	
+	public BaseTower getNewInstance(Coordinate loc)
+	{
+		return new TerraRaiser(loc,true);
+	}
+	
+
+	
+	public TerraRaiser(Coordinate loc,boolean isInstance)
+	{
+		this.setIsInstance(isInstance);
+		
+		this.setCost(10);
+		
+		this.setMaxHealth(10);
+		this.setUpgradeHealth(new int[]{5,10,15});
+		this.setCurrentHealth(this.getMaxHealth());
+		
+		this.setMaxUpgradeLevel(3);
+		this.setUpgradeCost(new int[]{10,15,20});
+		
+		this.setLoc(loc);
+		
+		this.setUrl("towers/terraraiser.png");
+		
+		// ------------------ TOWER INFO/DESCRIPTION ------------------------
+		
+		this.setTowerName(new String[] {
+				"Terraraiser",
+				"Terraraiser II",
+				"Terraraiser III",
+				"Terraraiser IV"
+		});
+		
+		this.setTowerDescription(new String[] {
+				"Creates a sturdy wall which absorbs damage.",
+				"Creates a sturdy wall which absorbs damage.",
+				"Creates a sturdy wall which absorbs damage.",
+				"Creates a sturdy wall which absorbs damage.",
+		});
+		
+		this.setTowerUpgradeDescription(new String[] {
+				"Raises a wall from earth, protecting towers behind it.",
+				"Increases toughness of the wall.",
+				"Greatly increases toughness of the wall.",
+				"Massively increases toughness of the wall.",
+		});
+		
+		this.initialize();
+		
+	}
+	
+
+}
