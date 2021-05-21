fbf51e39b107137d3088abbaeb045936135beca8
palmcm <palmermonkeyboy@gmail.com>
2021-05-22 01:59:22 +0700

    javadoc for TowerDesBox

--- src/gui/component/TowerDesBox.java
+++ src/gui/component/TowerDesBox.java
@@ -2,22 +2,26 @@ package gui.component;
 
 import config.GameConfig;
 import javafx.geometry.Insets;
-import javafx.scene.layout.Border;
-import javafx.scene.layout.BorderStroke;
-import javafx.scene.layout.BorderStrokeStyle;
-import javafx.scene.layout.BorderWidths;
-import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import logic.towers.BaseTower;
 import utils.FontUtil;
 
+/**
+ * 
+ * Box for display tower information in PickTowerScene
+ *
+ */
 public class TowerDesBox extends VBox{
+	/** Tower name*/
 	private Text towerName;
+	/** Tower description*/
 	private Text towerDes;
 	
+	/**
+	 * Constructor for TowerDesBox
+	 */
 	public TowerDesBox() {
 		towerName = new Text();
 		towerName.setFont(FontUtil.loadFont(48));
@@ -34,17 +38,26 @@ public class TowerDesBox extends VBox{
 		this.setMinSize(600,250);
 //		this.setPrefHeight(180);
 	}
-	
+	/**
+	 * Set to display custom name and description
+	 * @param towerName Tower name or Title
+	 * @param towerDes Tower Description or Detail
+	 */
 	public void setTowerData(String towerName, String towerDes) {
 		this.towerName.setText(towerName);
 		this.towerDes.setText(towerDes);
 	}
-	
+	/**
+	 * Set to display tower name and description
+	 * @param tower Tower in the cell
+	 */
 	public void setTowerData(BaseTower tower) {
 		this.towerName.setText(tower.getCurrentName());
 		this.towerDes.setText(tower.getToolTipString());
 	}
-	
+	/**
+	 * Set to display default info
+	 */
 	public void setTowerDefault() {
 		this.towerName.setText("Tower Info");
 		this.towerDes.setText("Hover over a tower to view its information.");
